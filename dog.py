1d9746be8e03d1534ff5ae0419882e467e018ba5
github_username <you@example.com>
2021-05-28 16:28:57 +0300

    first comit

--- dog.py
+++ dog.py
@@ -8,5 +8,7 @@ class Dog:
         self.age= age
 
     def bite(self):
-        return f"My dog is a German shepherd it has a nice {self.color}, height of {self.height} and a weight of {self.weight} when it was {self.age} years old He bit someone"   
+        return f"My dog is a German shepherd it has a nice {self.color}, height of {self.height} and a weight of {self.weight} when it was {self.age} years old He bit someone" 
+    def eat(self):
+        return f"Her dog loves to eat it is a {self.color} German shepherd with a weight of {self.weight} a height of {self.weight}. The dog just turned {self.age} years old"      
         
